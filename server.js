88d42cc55ac92d24d8625922a5d35ab662399ebe
Paloma-Yao <zero-in-ur-area@sjtu.edu.cn>
2026-03-15 02:28:31 +0800

    Fix Railway root route

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const publicDir = path.join(__dirname, 'public');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(publicDir));
+app.get('/', (_req, res) => {
+    res.sendFile(path.join(publicDir, 'index.html'));
+});
 
 const MIN_PLAYERS = 3;
 
